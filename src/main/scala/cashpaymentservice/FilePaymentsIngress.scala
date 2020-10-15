2adf3d54619d66ea03ff2c796e95a7e4d0f1cf70
skostenko <skostenko@neoflex.ru>
2020-10-15 11:59:54 +0300

    added file upload from directory in application.conf logic

--- src/main/scala/cashpaymentservice/FilePaymentsIngress.scala
+++ src/main/scala/cashpaymentservice/FilePaymentsIngress.scala
@@ -1,27 +1,40 @@
 package cashpaymentservice
 
+import java.io.{File, FileNotFoundException}
 import java.nio.file.Path
 
-import akka.stream.scaladsl.{FileIO, Framing, RunnableGraph, Sink}
+import akka.stream.scaladsl.{FileIO, Framing, RunnableGraph}
 import akka.util.ByteString
 import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
 import cloudflow.akkastream.{AkkaStreamlet, AkkaStreamletLogic}
-import cloudflow.streamlets.{RoundRobinPartitioner, StreamletShape}
 import cloudflow.streamlets.avro.AvroOutlet
-
+import cloudflow.streamlets.{RoundRobinPartitioner, StreamletShape}
 
 class FilePaymentsIngress extends AkkaStreamlet {
 
-  val out = AvroOutlet[PaymentData]("out").withPartitioner(RoundRobinPartitioner)
+  val paymentsOut: AvroOutlet[PaymentData] = AvroOutlet[PaymentData]("paymentsOut").withPartitioner(RoundRobinPartitioner)
 
-  override def shape(): StreamletShape = StreamletShape.withOutlets(out)
+  override def shape(): StreamletShape = StreamletShape.withOutlets(paymentsOut)
 
   override protected def createLogic(): AkkaStreamletLogic = new RunnableGraphStreamletLogic() {
+
+    val fileDirectory: String = system.settings.config.getString("payments.directory")
+    val fileName:String = system.settings.config.getString("payments.filename")
+
+    def paymentsPath(fileDirectory: String, fileName: String): Path = {
+      val paymentsPath = fileDirectory + File.pathSeparator + fileName
+      val paymentsFile = new File(paymentsPath)
+      if (paymentsFile.exists)
+        paymentsFile.toPath
+      else
+        throw new FileNotFoundException(s"File with directory $paymentsPath not found")
+    }
+
     override def runnableGraph(): RunnableGraph[_] =
-      FileIO.fromPath(Path.of("/test-data/payments.dat"))
+      FileIO.fromPath(paymentsPath(fileDirectory,fileName))
         .via(Framing.delimiter(ByteString("\n"), 1024))
-        .map(_.utf8String)
-        .to(plainSink(out))
+        .map(s => PaymentData(s.utf8String))
+        .to(plainSink(paymentsOut))
   }
 
 }
