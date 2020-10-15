77614d57eef567e74bc0c88fd28471abacacdff3
skostenko <skostenko@neoflex.ru>
2020-10-15 12:14:12 +0300

    added bug fix

--- src/main/scala/cashpaymentservice/PaymentLoggingEgress.scala
+++ src/main/scala/cashpaymentservice/PaymentLoggingEgress.scala
@@ -1,6 +1,7 @@
 package cashpaymentservice
 
-import akka.stream.scaladsl.{RunnableGraph, Source}
+import akka.event.LoggingAdapter
+import akka.stream.scaladsl.{RunnableGraph, Sink}
 import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
 import cloudflow.akkastream.util.scaladsl.Merger
 import cloudflow.akkastream.{AkkaStreamlet, AkkaStreamletLogic}
@@ -9,20 +10,20 @@ import cloudflow.streamlets.avro.AvroInlet
 
 class PaymentLoggingEgress extends AkkaStreamlet{
 
-  val checkIn = AvroInlet[PaymentStatus]("checkIn")
-  val statusIn = AvroInlet[PaymentStatus]("statusIn")
+  val checkIn: AvroInlet[PaymentStatus] = AvroInlet[PaymentStatus]("checkIn")
+  val statusIn: AvroInlet[PaymentStatus] = AvroInlet[PaymentStatus]("statusIn")
 
   override def shape(): StreamletShape = StreamletShape.withInlets(checkIn,statusIn)
 
   override protected def createLogic(): AkkaStreamletLogic = new RunnableGraphStreamletLogic() {
 
-    val log = system.log
+    val log: LoggingAdapter = system.log
 
     override def runnableGraph(): RunnableGraph[_] = {
       Merger.source(checkIn,statusIn).map(s => s.infoType match {
         case "WARN" => log.warning(s.message)
         case "INFO" => log.info(s.message)
-      })
+      }).to(Sink.ignore)
     }
   }
 
