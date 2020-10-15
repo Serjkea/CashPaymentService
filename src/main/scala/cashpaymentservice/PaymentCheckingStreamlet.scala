bc0b4bc73d11e809fdf4570da6d96332a2ced7a4
skostenko <skostenko@neoflex.ru>
2020-10-15 22:26:51 +0300

    added PaymentCheckingStreamlet logic

--- src/main/scala/cashpaymentservice/PaymentCheckingStreamlet.scala
+++ src/main/scala/cashpaymentservice/PaymentCheckingStreamlet.scala
@@ -4,22 +4,52 @@ import cloudflow.flink._
 import cloudflow.streamlets.StreamletShape
 import cloudflow.streamlets.avro.{AvroInlet, AvroOutlet}
 import org.apache.flink.api.scala.createTypeInformation
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag}
+import org.apache.flink.util.Collector
 
 class PaymentCheckingStreamlet extends FlinkStreamlet {
 
-  val paymentIn = AvroInlet[PaymentData]("paymentIn")
+  @transient val paymentIn: AvroInlet[PaymentData] = AvroInlet[PaymentData]("paymentIn")
 
-  val paymentStatusOut = AvroOutlet[PaymentStatus]("paymentStatusOut")
-  val validPaymentOut = AvroOutlet[ValidPayment]("validPaymentOut")
+  @transient val paymentStatusOut: AvroOutlet[PaymentStatus] = AvroOutlet[PaymentStatus]("paymentStatusOut")
+  @transient val validPaymentOut: AvroOutlet[ValidPayment] = AvroOutlet[ValidPayment]("validPaymentOut")
 
   override def shape(): StreamletShape = StreamletShape(paymentIn).withOutlets(paymentStatusOut,validPaymentOut)
 
   override protected def createLogic(): FlinkStreamletLogic = new FlinkStreamletLogic() {
     override def buildExecutionGraph(): Unit = {
+
+      val outputTag = new OutputTag[PaymentStatus]("warning-branch")
+
       val inputPayment: DataStream[PaymentData] = readStream(paymentIn)
-      val outputPayment: DataStream[ValidPayment] = ???
+      val outputValidPayment = inputPayment.process((paymentData: PaymentData, ctx: ProcessFunction[PaymentData, ValidPayment]#Context, out: Collector[ValidPayment]) => {
+        val payment = buildValidPayment(paymentData)
+        if (isValidPayment(payment))
+          out.collect(payment)
+        else
+          ctx.output(outputTag, PaymentStatus("WARN", s"Payment ${paymentData.payment} is not valid!"))
+      })
+
+      val outputPaymentStatus = outputValidPayment.getSideOutput(outputTag)
+
+      writeStream(validPaymentOut,outputValidPayment)
+      writeStream(paymentStatusOut,outputPaymentStatus)
+
     }
   }
+
+  def buildValidPayment(inputPayment: PaymentData): ValidPayment = {
+    val wordPattern = "\\w+".r
+    val namePattern = "[a-zA-Z]+".r
+    val numberPattern = "[0-9]+".r
+    val fields = (wordPattern findAllIn inputPayment.payment).toSeq
+    if ((fields.size == 3) && fields(0).matches(namePattern.regex) && fields(1).matches(namePattern.regex) && fields(2).matches(numberPattern.regex))
+      ValidPayment(fields(0),fields(1),fields(2).toInt)
+    else
+      ValidPayment("","",-1)
+  }
+
+  def isValidPayment(payment: ValidPayment): Boolean = payment.value != -1
 
 }
