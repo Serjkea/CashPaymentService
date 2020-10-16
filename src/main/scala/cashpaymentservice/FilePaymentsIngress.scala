605379fff6fe94ad75cfaeb364c57017280c590e
skostenko <skostenko@neoflex.ru>
2020-10-16 16:43:46 +0300

    changed the logic of reading files

--- src/main/scala/cashpaymentservice/FilePaymentsIngress.scala
+++ src/main/scala/cashpaymentservice/FilePaymentsIngress.scala
@@ -1,14 +1,19 @@
 package cashpaymentservice
 
-import java.io.{File, FileNotFoundException}
+import java.nio.file
 import java.nio.file.Path
 
-import akka.stream.scaladsl.{FileIO, Framing, RunnableGraph}
+import akka.NotUsed
+import akka.stream.IOResult
+import akka.stream.alpakka.file.scaladsl.Directory
+import akka.stream.scaladsl.{FileIO, Framing, RunnableGraph, Source}
 import akka.util.ByteString
 import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
 import cloudflow.akkastream.{AkkaStreamlet, AkkaStreamletLogic}
 import cloudflow.streamlets.avro.AvroOutlet
-import cloudflow.streamlets.{RoundRobinPartitioner, StreamletShape}
+import cloudflow.streamlets.{ReadWriteMany, RoundRobinPartitioner, StreamletShape, VolumeMount}
+
+import scala.concurrent.Future
 
 class FilePaymentsIngress extends AkkaStreamlet {
 
@@ -16,25 +21,31 @@ class FilePaymentsIngress extends AkkaStreamlet {
 
   override def shape(): StreamletShape = StreamletShape.withOutlets(paymentsOut)
 
+  val fileDirectory: String = context.system.settings.config.getString("payments.directory")
+  val fileName:String = context.system.settings.config.getString("payments.filename")
+
+  private val sourceData = VolumeMount(fileName, fileDirectory, ReadWriteMany)
+  override def volumeMounts = Vector(sourceData)
+
   override protected def createLogic(): AkkaStreamletLogic = new RunnableGraphStreamletLogic() {
 
-    val fileDirectory: String = system.settings.config.getString("payments.directory")
-    val fileName:String = system.settings.config.getString("payments.filename")
-
-    def paymentsPath(fileDirectory: String, fileName: String): Path = {
-      val paymentsPath = fileDirectory + File.pathSeparator + fileName
-      val paymentsFile = new File(paymentsPath)
-      if (paymentsFile.exists)
-        paymentsFile.toPath
-      else
-        throw new FileNotFoundException(s"File with directory $paymentsPath not found")
+    val listFiles: NotUsed ⇒ Source[file.Path, NotUsed] = { _ ⇒
+      Directory.ls(getMountedPath(sourceData))
+    }
+    val readFile: Path ⇒ Source[ByteString, Future[IOResult]] = { path: Path ⇒
+      FileIO.fromPath(path)
     }
 
-    override def runnableGraph(): RunnableGraph[_] =
-      FileIO.fromPath(paymentsPath(fileDirectory,fileName))
+    override def runnableGraph(): RunnableGraph[_] = {
+      Source.single(NotUsed)
+        .flatMapConcat(listFiles)
+        .flatMapConcat(readFile)
         .via(Framing.delimiter(ByteString("\n"), 1024))
         .map(s => PaymentData(s.utf8String))
         .to(plainSink(paymentsOut))
+    }
+
   }
+
 
 }
